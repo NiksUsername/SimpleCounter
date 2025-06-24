39f81041759676e5fd8ad5d8ca1da811d6246493
Nick <Mykola.Biloshapka@lnu.edu.ua>
2025-06-24 23:05:01 +0300

    Init commit

---
+++ app/src/main/java/com/niks/counter/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.niks.counter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.niks.counter.ui.theme.CounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CounterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ToolPanel() {
+    Row(
+        
+    ) {  }
+}
+
+@Composable
+fun CounterContainer() {
+    var clickCount by remember { mutableIntStateOf(0) }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text (
+            text = String.format("%04d", clickCount)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+
+}
